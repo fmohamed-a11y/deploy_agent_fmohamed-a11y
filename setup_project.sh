e0aa80624d572963e18989075915631d780bf089
Fatima <f.mohamed@alustudent.com>
2026-06-10 21:42:32 +0200

    replaced files with source files

--- setup_project.sh
+++ setup_project.sh
@@ -12,7 +12,7 @@ cleanup() {
 trap cleanup SIGINT 
 mkdir -p "${TARGET_DIR}/Helpers"
 mkdir -p "${TARGET_DIR}/reports"
-touch "${TARGET_DIR}/attendance_checker.py"
-touch "${TARGET_DIR}/Helpers/assets.csv"
-touch "${TARGET_DIR}/Helpers/config.json"
-touch "${TARGET_DIR}/reports/reports.log"
+cp attendance_checker.py "${TARGET_DIR}/attendance_checker.py"
+cp assets.csv "${TARGET_DIR}/Helpers/assets.csv"
+cp config.json "${TARGET_DIR}/Helpers/config.json"
+cp reports.log "${TARGET_DIR}/reports/reports.log"
