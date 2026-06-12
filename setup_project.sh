5a367b179bf287f5758f784c26497b10e1750df4
Fatima <f.mohamed@alustudent.com>
2026-06-12 17:49:34 +0200

    added sed and conditions for failure and warnings

--- setup_project.sh
+++ setup_project.sh
@@ -16,10 +16,24 @@ cp attendance_checker.py "${TARGET_DIR}/attendance_checker.py"
 cp assets.csv "${TARGET_DIR}/Helpers/assets.csv"
 cp config.json "${TARGET_DIR}/Helpers/config.json"
 cp reports.log "${TARGET_DIR}/reports/reports.log"
-read -p "Updated Warning/Failure thresholds?(Y/N): " ANSWER
+read -p "Do you want to update the warning and failure thresholds? (Y/N): " ANSWER
 ANSWER=$(echo "$ANSWER" | tr '[:upper:]' '[:lower:]')
 if [ "$ANSWER" = "y" ]; then
 	echo "yes"
+read -p "Please enter the new warning threshold: " warn_val
+if [[ "$warn_val" =~ ^[0-9]+$ ]]; then
+	echo "valid"
+sed -i 's/"warning": [0-9]*/"warning": '"$warn_val"'/' "${TARGET_DIR}/Helpers/config.json"
+else
+	echo "invalid"
+fi
+read -p "Please enter the new failure threshold: " fail_val 
+if [[ "$fail_val" =~ ^[0-9]+$ ]]; then
+	echo "valid"
+ sed -i 's/"failure": [0-9]*/"failure": '"$fail_val"'/' "${TARGET_DIR}/Helpers/config.json"
+else
+	echo "invalid"
+fi
 else
 	echo "no"
 fi
