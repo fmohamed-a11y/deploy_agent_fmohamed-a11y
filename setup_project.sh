aa394cc9a6e930b3aa554e18c7dc2408e7d47f82
Fatima <f.mohamed@alustudent.com>
2026-06-13 20:12:00 +0200

    copid the original file using EOF  and tested the file

--- setup_project.sh
+++ setup_project.sh
@@ -1,13 +1,14 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
 PROJECT_NAME="attendance_tracker"
 cleanup() {
-	if [ -d "${TARGET_DIR}" ]; then 
-		tar -czf "${TARGET_DIR}_archive.tar.gz" "${TARGET_DIR}"
-		rm -rf "${TARGET_DIR}"
-	fi
-	exit
+        echo "Interrupted! Archiving and cleaning up..."
+        if [ -d "${TARGET_DIR}" ]; then
+                tar -czf "${TARGET_DIR}_archive.tar.gz" "${TARGET_DIR}"
+                rm -rf "${TARGET_DIR}"
+        fi
+        exit
 }
-trap cleanup SIGINT 
+trap cleanup SIGINT
 read -p "Enter the project suffix (e.g. v1):" INPUT
 TARGET_DIR="${PROJECT_NAME}_${INPUT}"
 mkdir -p "${TARGET_DIR}/Helpers"
@@ -19,32 +20,32 @@ cp reports.log "${TARGET_DIR}/reports/reports.log"
 read -p "Do you want to update the warning and failure thresholds? (Y/N): " ANSWER
 ANSWER=$(echo "$ANSWER" | tr '[:upper:]' '[:lower:]')
 if [ "$ANSWER" = "y" ]; then
-	echo "yes"
+        echo "yes"
 read -p "Please enter the new warning threshold: " warn_val
 if [[ "$warn_val" =~ ^[0-9]+$ ]]; then
-	echo "valid"
+        echo "valid"
 sed -i 's/"warning": [0-9]*/"warning": '"$warn_val"'/' "${TARGET_DIR}/Helpers/config.json"
 else
-	echo "invalid"
+        echo "invalid"
 fi
-read -p "Please enter the new failure threshold: " fail_val 
+read -p "Please enter the new failure threshold: " fail_val
 if [[ "$fail_val" =~ ^[0-9]+$ ]]; then
-	echo "valid"
+        echo "valid"
  sed -i 's/"failure": [0-9]*/"failure": '"$fail_val"'/' "${TARGET_DIR}/Helpers/config.json"
 else
-	echo "invalid"
+        echo "invalid"
 fi
 else
-	echo "no"
+        echo "no"
 fi
-if command -v python3 > /dev/null 2>&1; then 
-	echo "Success: python3 is installed"
-	python3 --version
+if command -v python3 > /dev/null 2>&1; then
+        echo "Success: python3 is installed"
+        python3 --version
 else
-	echo "Warning: python3 is not installed"
+        echo "Warning: python3 is not installed"
 fi
 if [[ -f "${TARGET_DIR}/attendance_checker.py" ]] && [[ -d "${TARGET_DIR}/Helpers" ]] && [[ -d "${TARGET_DIR}/reports" ]]; then
-	echo "****Directory Structure is SUCCESSFULLY verified****"
+        echo "****Directory Structure is SUCCESSFULLY verified****"
 else
-	echo -e "******ALERT!!!******:\nSetup failed - directory structure is invalid"
+        echo -e "******ALERT!!!******:\nSetup failed - directory structure is invalid"
 fi
